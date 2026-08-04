9443e176c8c5e344caea05da05cbc0d316735c0e
CHEEDEELLA PAVAN KUMAR <chpavan6305@gmail.com>
2026-08-04 12:01:56 +0530

    Completed Programming vs Programming Language task

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/ProgrammingVsJava.java
@@ -0,0 +1,16 @@
+public class ProgrammingVsJava
+{
+    public static void main(String[] args) {
+        int percentage=72;
+        int activeBacklogs=0;
+
+        if(percentage>=60 && activeBacklogs==0)
+        {
+            System.out.println("Eligible");
+        }
+        else
+        {
+            System.out.println("Not Eligible");
+        }
+    }
+}
--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,8 +1,9 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Java Track!");
-        System.out.println("My name is CHEEDELLA PAVAN KUMAR.");
-        System.out.println("My career goal is to become a Java Full Stack Developer.");
-        System.out.println("I selected Java because it is platform independent, secure, and widely used for enterprise applications.");
+       //Write your code here
+       System.out.println("Welcome to Java!");
+       System.out.println("My name is Pavan");
+       System.out.println("My goal is to become a Java developer");
+       System.out.println("I selected java because I want to build large applications");    
     }
 }
